5102d9caea7e60b7c33cb83b785580f93a441aa3
Steve Harlan <StevenHarlan@gmail.com>
2016-10-10 16:28:56 -0700

    modified css of components; added mock data for service

--- src/app/polls-active.service.ts
+++ src/app/polls-active.service.ts
@@ -4,22 +4,44 @@ import { Injectable } from '@angular/core';
 export class PollsActiveService {
   currentPolls = [
   { id: 0,
+    presentation: 'Tech Talk',
+    event: 'codesmith night',
     type: 'MC',
-    question: `what's for dinner`,
+    question: `what's for dinner?`,
     prompts: {a: 'cheese', b: 'radish', c: 'beans', d: 'candy'},
-    results: {a: 200, b: 203, c: 45, d: 97}
+    results: {a: 200, b: 203, c: 45, d: 97},
+    createdDate: 1468046219000,
+    eventDate: 1477255207000
   },
   { id: 1,
+    presentation: 'Tech Talk',
+    event: 'codesmith night',
     type: 'MC',
-    question: `what's for breakfast`,
+    question: `what's for breakfast?`,
     prompts: {a: 'cheese', b: 'radish', c: 'beans', d: 'candy'},
-    results: {a: 200, b: 203, c: 45, d: 97}
+    results: {a: 200, b: 203, c: 45, d: 97},
+    createdDate: 1468046219000,
+    eventDate: 1477255207000
   },
   { id: 2,
+    presentation: 'Tech Talk',
+    event: 'codesmith night',
     type: 'MC',
-    question: `what's for lunch`,
-    prompts: {a: 'cheese', b: 'radish', c: 'beans', d: 'candy'},
-    results: {a: 200, b: 203, c: 45, d: 97}
+    question: `what's for lunch?`,
+    prompts: {a: 'cheese', b: 'radish', c: 'beans'},
+    results: {a: 200, b: 203, c: 45},
+    createdDate: 1468046219000,
+    eventDate: 1477255207000
+  },
+  { id: 3,
+    presentation: 'Tech Talk',
+    event: 'codesmith night',
+    type: 'MC',
+    question: `what's for the party?`,
+    prompts: {a: 'cheese', b: 'radish', c: 'beans'},
+    results: {a: 200, b: 203, c: 45},
+    createdDate: 1468046219000,
+    eventDate: 1477255207000
   }
 ];
 
